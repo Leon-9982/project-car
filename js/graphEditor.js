f025f215579443e8f10d87b663da6e6de495210b
Leon <probabyleon9982@gmail.com>
2024-10-03 13:56:26 +0100

    Add and remove points, with hover to select and highlight

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -1,42 +1,58 @@
 class GraphEditor {
-    constructor(canvas, graph) {
-        this.canvas = canvas;
-        this.graph = graph;
+  constructor(canvas, graph) {
+    this.canvas = canvas;
+    this.graph = graph;
 
-        this.ctx = this.canvas.getContext("2d");
+    this.ctx = this.canvas.getContext("2d");
 
-        this.selected = null;
-        this.hovered = null;
+    this.selected = null;
+    this.hovered = null;
 
-        this.addEventListeners();
-    }
+    this.#addEventListeners();
+  }
 
-    addEventListeners() {
-        this.canvas.addEventListener("mousedown", (evt) => {
-            const mouse = new Point(evt.offsetX, evt.offsetY);
-            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
-            if (this.hovered) {
-                this.selected = this.hovered;
-                return;
-            }
-            this.graph.addPoint(mouse);
-            this.selected = mouse;
-        })
-
-        this.canvas.addEventListener("mousemove", (evt) => {
-            const mouse = new Point(evt.offsetX, evt.offsetY);
-            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
-        })
-    }
-
-    display() {
-        this.graph.draw(this.ctx);
+  #addEventListeners() {
+    this.canvas.addEventListener("mousedown", (evt) => {
+      if (evt.button == 2) { //right click
         if (this.hovered) {
-            this.hovered.draw(this.ctx, { fill: true });
+          this.#removePoint(this.hovered);
         }
+      }
+      if (evt.button == 0) {
+        const mouse = new Point(evt.offsetX, evt.offsetY);
+        if (this.hovered) {
+          this.selected = this.hovered;
+          return;
+        }
+        this.graph.addPoint(mouse);
+        this.selected = mouse;
+        this.hovered = mouse;
+      }
+    })
 
-        if (this.selected) {
-            this.selected.draw(this.ctx, { outline: true });
-        }
+    this.canvas.addEventListener("mousemove", (evt) => {
+      const mouse = new Point(evt.offsetX, evt.offsetY);
+      this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+    })
+    this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault())
+  }
+
+  #removePoint(point) {
+    this.graph.removePoint(point)
+    this.hovered = null;
+    if (this.selected == point) {
+      this.selected = null;
     }
+  }
+
+  display() {
+    this.graph.draw(this.ctx);
+    if (this.hovered) {
+      this.hovered.draw(this.ctx, { fill: true });
+    }
+
+    if (this.selected) {
+      this.selected.draw(this.ctx, { outline: true });
+    }
+  }
 }
