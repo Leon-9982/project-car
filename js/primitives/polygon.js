7625e369af87522ec2e5a9b1640ee8cc49ebd977
Leon <probabyleon9982@gmail.com>
2024-10-06 11:29:38 +0100

    Remove debugging leftovers

--- js/primitives/polygon.js
+++ js/primitives/polygon.js
@@ -12,7 +12,7 @@ class Polygon {
   static break(poly1, poly2) {
     const segs1 = poly1.segments;
     const segs2 = poly2.segments
-    const intersections = []
+
 
     for (let i = 0; i < segs1.length; i++) {
       for (let j = 0; j < segs2.length; j++) {
@@ -21,7 +21,6 @@ class Polygon {
         )
         if (int && int.offset != 1 && int.offset != 0) {
           const point = new Point(int.x, int.y);
-          intersections.push(point);
           let aux = segs1[i].p2
           segs1[i].p2 = point
           segs1.splice(i + 1, 0, new Segment(point, aux));
@@ -32,12 +31,11 @@ class Polygon {
         }
       }
     }
-    return intersections;
   }
 
   drawSegments(ctx) {
     for (const seg of this.segments) {
-      seg.draw(ctx, { color: getRandomColor() });
+      seg.draw(ctx, { color: getRandomColor(), width: 5 });
     }
   }
 
