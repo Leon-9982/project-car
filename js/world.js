0da3a5a3d2f9f450a167084a202a23b6c9c1f9f9
Leon <probabyleon9982@gmail.com>
2024-10-06 11:15:12 +0100

    Intersection(junction) detection and mergers

--- js/world.js
+++ js/world.js
@@ -16,10 +16,19 @@ class World {
         new Envelope(seg, this.roadWidth, this.roadRoundness)
       )
     }
+
+    this.intersections = Polygon.break(
+      this.envelopes[0].poly,
+      this.envelopes[1].poly
+    )
   }
   draw(ctx) {
     for (const env of this.envelopes) {
       env.draw(ctx);
+    }
+
+    for (const int of this.intersections) {
+      int.draw(ctx);
     }
   }
 }
